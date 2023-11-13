dad8c18054f01e9cb40ca12c1f544088311ba721
Aksh <toakshh@gmail.com>
2023-11-13 14:01:18 +0530

    island rotation added with some minor changes

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import birdScene from "../assets/3d/bird.glb";
 const Bird = () => {
   const { scene, animations } = useGLTF(birdScene);
+  // [-5, 2, 1] [-25, 10, -10]
   return (
     <mesh position={[-5, 2, 1]} scale={[0.03, 0.003, 0.003]}>
       <primitive object={scene} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
 import Island from "../models/Island";
@@ -11,27 +11,45 @@ import Plane from "../models/Plane";
 </div> */
 }
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
+
   // Scales 3d model for diffrent screensize devices
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
-    let screenPosition = [0, -13, -42];
+    let screenPosition = [0, -4.5, -43];
     let rotation = [0.1, 4.7, 0];
 
     if (window.innerWidth < 768) {
       screenScale = [0.9, 0.9, 0.9];
     } else {
-      screenScale = [1, 1, 1];
+      screenScale = [1.2, 1.2, 1.2];
     }
     return [screenScale, screenPosition, rotation];
   };
 
+  const adjustPlaneForScreenSize = () => {
+    let screenScale;
+    let screenPosition;
+
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+    return [screenScale, screenPosition];
+  };
+
   const [islandScale, islandPosition, islandRotation] =
     adjustIslandForScreenSize();
-
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
   return (
     <section className="w-full h-screen relative">
       <Canvas
-        className="w-full h-screen bg-transparent"
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
         camera={{ near: 0.1, far: 1000 }}
       >
         {/* Show loader while 3d model is loading. NOTE: 3d models can't understand html elements. Use Drei */}
@@ -50,9 +68,16 @@ const Home = () => {
           <Island
             position={islandPosition}
             scale={islandScale}
-            rotatioe={islandRotation}
+            rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
           />
-          <Plane />
+          <Plane
+            planePosition={planePosition}
+            planeScale={planeScale}
+            isRotating={isRotating}
+            rotation={[0, 20, 0]}
+          />
         </Suspense>
       </Canvas>
     </section>
