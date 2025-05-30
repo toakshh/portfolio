f75414f00e132960469dcb3e931b0d3d765f52ca
Akshat <akshnnd@gmail.com>
2025-05-30 12:16:16 +0530

    added react native project

--- src/constants/index.js
+++ src/constants/index.js
@@ -149,19 +149,19 @@ export const experiences = [
             "Participating in code reviews and providing constructive feedback to other developers.",
         ],
     },
-    // {
-    //     title: "React Native Developer",
-    //     company_name: "Tesla",
-    //     icon: tesla,
-    //     iconBg: "#fbc3bc",
-    //     date: "Jan 2021 - Feb 2022",
-    //     points: [
-    //         "Developing and maintaining web applications using React.js and other related technologies.",
-    //         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-    //         "Implementing responsive design and ensuring cross-browser compatibility.",
-    //         "Participating in code reviews and providing constructive feedback to other developers.",
-    //     ],
-    // }
+    {
+        title: "React Native Developer",
+        company_name: "Freelancer",
+        icon: tesla,
+        iconBg: "#fbc3bc",
+        date: "Currently Working",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    }
 ];
 
 export const socialLinks = [
@@ -183,6 +183,13 @@ export const socialLinks = [
 ];
 
 export const projects = [
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-black',
+        name: 'MovieMania',
+        description: 'Developed a React native app using expo and appwrite. More details on Github and resume.',
+        link: 'https://github.com/toakshh/movieApp',
+    },
     {
         iconUrl: pricewise,
         theme: 'btn-back-red',
